b0cc357663cabe153f93257bf906950e21424951
srmtwenty <srmcho@hotmail.com>
2021-02-07 12:00:11 -0800

    Your message about the commit

---
+++ belt_prac1/server/models/post.model.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+
+const PostSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: [true, "{PATH} is required."],
+      minlength: [2, "{PATH} must be at least {MINLENGTH} characters."],
+    },
+    description: {
+      type: String,
+      required: [true, "{PATH} is required."],
+      minlength: [2, "{PATH} must be at least {MINLENGTH} characters."],
+    },
+
+    imgUrl: {
+      type: String,
+      required: [true, "{PATH} is required."],
+    },
+    primaryCategory: {
+      type: String,
+      required: [true, "{PATH} is required."],
+      minlength: [2, "{PATH} must be at least {MINLENGTH} characters."],
+    },
+    secondaryCategory: {
+      type: String,
+      required: [true, "{PATH} is required."],
+      minlength: [2, "{PATH} must be at least {MINLENGTH} characters."],
+    },
+  },
+  { timestamps: true }
+);
+
+// register our model and enforce the schema
+const Post = mongoose.model("Post", PostSchema);
+
+module.exports = Post;
